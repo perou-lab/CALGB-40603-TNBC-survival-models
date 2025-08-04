2871ad66434be23b0441b30bb4be9aeb9df8a3d3
Brooke Felsheim <brookefelsheim@gmail.com>
2025-08-04 17:11:29 -0400

    Scripts with arguments used to train each type of model

---
+++ model_scripts/run_stage_RNA_DNA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Rscript train_elastic_net.R \
+	--featureDataFile input_data/combined_molecular_clinical_feature_data.rds \
+	--predictorTypeFile input_data/model_predictor_types.tsv \
+	--includeTypes Stage,DNA,RNA \
+	--nBootstraps 100 \
+	--alphas 0.1,0.9,0.1 \
+	--lambdas 2,-2,20 \
+	--seed 822 \
+	--outputDir output/stage_RNA_DNA
